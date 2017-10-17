e96815ae18e90b5300f67839ab6388dbd5086359
Geo Badita <geo.badita@gmail.com>
2017-10-17 08:49:11 +0300

    First commit

--- lab3Co/lab3.py
+++ lab3Co/lab3.py
@@ -1,9 +1,27 @@
+import math
+
+def readInt(text):
+    '''
+    Tell if the user input is valid
+    text - user input
+    return the number the user introduces if it's an integer, else an
+    exception is thrown
+    '''
+    while True:
+        try:
+            number = int(input(text))
+            return number
+        except ValueError:
+            print("Valoare invalida")
+
 def gcd(a, b):
     '''
     Compute the greatest common divisor of a and b
     a, b positive integers
     return a number, the greatest common divisor of a and b
     '''
+    if a < 0 or b < 0:
+        return "Eroare"
     if b == 0:
         return a
     else:
@@ -20,13 +38,14 @@ def testGcd():
     assert gcd(2, 4) == 2
     assert gcd(4, 20) == 4
     assert gcd(256, 16) == 16
+    assert gcd(-2, 9) == "Eroare"
 
 
 def readList():
     l = []
-    n = int(input("Dati numarul de elemente al listei: "))
+    n = readInt("Dati numarul de elemente al listei: ")
     for i in range(0, n):
-        a = int(input("Introduceti un numar si apasati tasta enter... "))
+        a = readInt(("Introduceti elementul "+str(i+1)+":"))
         l.append(a)
     return l
 
@@ -37,6 +56,9 @@ def findLongestCoprime(myList):
     myList - list of integers
     return a list representing the longest sequence of coprime numbers
     '''
+    for i in range(0, len(myList)):
+        if myList[i] < 0:
+            return "Nu se poate calcula cel mai mare divizor comun a doua numere negative"
     maxLength = 0
     position = 0
     for i in range(0, len(myList)):
@@ -47,6 +69,9 @@ def findLongestCoprime(myList):
         if length > maxLength:
             maxLength = length
             position = i
+
+    if maxLength <= 0:
+        return []
     return myList[position - maxLength:position + 1]
 
 
@@ -69,6 +94,10 @@ def commonDigits(a, b):
     return True if a and b have at least 2 distinct common digits otherwise
     False
     '''
+    if a < 0:
+        a *= -1
+    if b < 0:
+        b *= -1
     newA = 0
     while a:
         newA |= (1 << a % 10)
@@ -117,6 +146,9 @@ def longestCommonDigits(myList):
             position = i
             maxLength = length
 
+    if maxLength <= 0:
+        return []
+
     return myList[position - maxLength:position + 1]
 
 
@@ -132,12 +164,79 @@ def testLongestCommonDigits():
     assert longestCommonDigits([]) == []
     assert longestCommonDigits([121, 55, 556, 65, 665, 211]) == [556, 65, 665]
 
+def isPrime(x):
+    '''
+    Verify if a number is prime
+    x - positive integer
+    returns True if x is prime, False otherwise
+    '''
+    if x < 2:
+        return  False
+    if x == 2:
+        return  True
+    if x % 2 == 0:
+        return  False
+    for i in range(3, int(math.sqrt(x)), 2):
+        if x % i == 0:
+            return False
+    return  True
+
+def testIsPrime():
+    '''
+    Function to check if the isPrime function works properly
+    no parameters
+    Stops the program if one of the assert statements is False,
+    otherwise nothing happens
+    '''
+    assert isPrime(2) == True
+    assert isPrime(4) == False
+    assert isPrime(-2) == False
+    assert isPrime(5) == True
+    assert isPrime(11) == True
+    assert isPrime(0) == False
+
+
+def longestPrime(myList):
+    '''
+    Find the longest sequence of prime numbers
+    myList - list of integers
+    return a list representing the longest sequence of prime numbers
+    '''
+    maxLength = 0
+    position = 0
+    for i in range(0, len(myList)):
+        length = 0
+        while i + 1 < len(myList) and isPrime(myList[i]) and isPrime(myList[i + 1]) == True:
+            length += 1
+            i += 1
+        if length > maxLength:
+            position = i
+            maxLength = length
+
+    if maxLength <= 0:
+        return []
+
+    return myList[position - maxLength:position + 1]
+
+def testLongestPrime():
+    '''
+    Function to check if longestPrime function works properly
+    no parameters
+    Doesen't return  anything
+    '''
+    assert longestPrime([1, 2, 3, 5, 7, 12]) == [2, 3, 5, 7]
+    assert longestPrime([]) == []
+    assert longestPrime([1, 1, 2, 2, 3, 5, 7]) == [2, 2, 3, 5, 7]
+    assert longestPrime([13, 13, 13, 13, 13]) == [13, 13, 13, 13, 13]
+
 def generalTest():
     '''
     Function to check if all the functions works properly
     no parameters
     Doesen't return  anything
     '''
+    testLongestPrime()
+    testIsPrime()
     testCommonDigits()
     testFindLongestCoprime()
     testGcd()
@@ -152,7 +251,8 @@ def printMenu():
     print("Apasati tasta 1 pentru a introduce un sir de numere... ")
     print("Apasati tasta 2 pentru a printa cea mai lunga secventa de numere prime intre ele... ")
     print("Apasati tasta 3 pentru a printa cea mai lunga secventa de numere care au cel putin doua cifre distincte... ")
-    print("Apasati tasta 4 pentru a inchide aplicatia... ")
+    print("Apasati tasta 4 pentru a printa cea mai lunga secventa de numere prime")
+    print("Apasati tasta 5 pentru a inchide aplicatia... ")
 
 
 def menuController():
@@ -164,7 +264,7 @@ def menuController():
     userList = []
     while True:
         printMenu()
-        command = int(input())
+        command = readInt("")
         if command == 1:
             userList = readList()
         elif command == 2:
@@ -178,6 +278,11 @@ def menuController():
                 continue
             print(longestCommonDigits(userList))
         elif command == 4:
+            if len(userList) == 0:
+                print("Trebuie mai intai sa introduceti o lista de numere... ")
+                continue
+            print(longestPrime(userList))
+        elif command == 5:
             exit()
         else:
             print("Valoarea introdusa nu este corecta, va rugam incercati din nou... ")
@@ -189,7 +294,9 @@ def mainFunction():
     Doesen't return anything, it just controls the entire program
     '''
     generalTest() #Test all functions to be sure they work properly
-    menuController() #The function which controls the user menu
+    menuController() #The function which controls the user console
+
+
 
 
 
@@ -198,3 +305,5 @@ THE CALL OF THE MAIN FUNCTION
 '''
 
 mainFunction()
+
+
